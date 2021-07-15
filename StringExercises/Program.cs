7ea805dedc56ee3ebb5cf6c86b695055536c8af3
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-07-15 17:42:24 -0500

    finished string practice

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,12 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string passage = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            string[] words = passage.Split(' ');
+            string[] sentences = passage.Split('.');
+            Console.WriteLine(string.Join(",", words));
+            Console.WriteLine(string.Join("/", sentences));
+            
         }
     }
 }
