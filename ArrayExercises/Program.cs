3a875a3fa4b8a9489683c3ac1f412a8fa9f6419f
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-07-15 17:04:46 -0500

    finished array practice

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -7,6 +7,13 @@ namespace ArrayExercises
         static void Main(string[] args)
         {
             int[] numbers = { 1, 1, 2, 3, 5, 8 };
+            foreach (int number in numbers)
+            {
+                if(number % 2 != 0)
+                {
+                    Console.WriteLine(number);
+                }
+            }
         }
     }
 }
