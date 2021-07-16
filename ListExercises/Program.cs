521f06b51cb192cf5c8005b736bd331b8bdedded
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-07-15 19:31:04 -0500

    almost finished with 3 and 4 of list exercises

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -19,7 +19,36 @@ namespace ListExercises
                 }
                 return totalEvens;
             }
+            
             Console.WriteLine(sumOfEvens(exampleNumbers));
+            
+            
+            string passage = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+            string[] splitPassage = passage.Split(' ');
+            List<string> wordsList = new List<string>();
+            foreach (string word in splitPassage)
+            {
+                wordsList.Add(word);
+            }
+            Console.WriteLine("Enter the word length for the search: ");
+            
+            static List<string> onlyFiveLetters(List<string> wordsList)
+            {
+                string input = Console.ReadLine();
+                int wordLength = int.Parse(input);
+                List<string> fiveLetterWords = new List<string>();
+                foreach (string word in wordsList)
+                {
+                    if(word.Length == wordLength)
+                    {
+                        fiveLetterWords.Add(word);
+                        Console.WriteLine(word);
+                    }
+                }
+                return fiveLetterWords;
+            }
+
+            Console.WriteLine(onlyFiveLetters(wordsList));
         }
     }
 }
