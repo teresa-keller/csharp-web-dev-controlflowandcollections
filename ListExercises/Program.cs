76d222adba2208d76290d60c810cea0518db69db
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2021-07-21 11:51:35 -0500

    bonus mission

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
 namespace ListExercises
 {
     class Program
@@ -22,7 +24,6 @@ namespace ListExercises
             
             Console.WriteLine(sumOfEvens(exampleNumbers));
             
-            
             string passage = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
             string[] splitPassage = passage.Split(' ');
             List<string> wordsList = new List<string>();
@@ -30,6 +31,8 @@ namespace ListExercises
             {
                 wordsList.Add(word);
             }
+            string otherPassage = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            List<string> otherSplitPassage = new List<string>(otherPassage.Split(' ').ToList());
             Console.WriteLine("Enter the word length for the search: ");
             
             static List<string> onlyFiveLetters(List<string> wordsList)
@@ -48,7 +51,10 @@ namespace ListExercises
                 return fiveLetterWords;
             }
 
-            Console.WriteLine(onlyFiveLetters(wordsList));
+            Console.WriteLine(onlyFiveLetters(otherSplitPassage));
+            
+            
         }
+        
     }
 }
